799a3eb18d472297f7f30e6747507012951acb5f
obb8923 <obb8923@naver.com>
2025-10-03 20:56:47 +0900

    .

--- src/domain/App/component/memoButton.tsx
+++ src/domain/App/component/memoButton.tsx
@@ -1,5 +1,6 @@
 import { TouchableOpacity } from 'react-native';
 import React from 'react';
+import Animated, { LinearTransition } from 'react-native-reanimated';
 import { Text } from '@component/Text';
 import {LiquidGlassView} from '@component/LiquidGlassView';
 import {todayMonth, todayDay} from '@constant/Date';
@@ -11,11 +12,13 @@ interface MemoButtonProps {
 
 export const MemoButton = ({ onPress, date }: MemoButtonProps) => {
   return (
-    <LiquidGlassView style={{borderRadius: 1000}}>
-      <TouchableOpacity onPress={onPress} className="flex-row px-4 py-2 justify-center items-center">
-        <Text text={`${date?.month === todayMonth && date?.day === todayDay ? '오늘' : `${date?.month}월 ${date?.day}일`}의 기록`} type="body3" className="mr-2" />
-        <FileIcon width={14} height={16} color={'white'}/>
-      </TouchableOpacity>
-    </LiquidGlassView>
+      <AnimatedLiquidGlassView layout={LinearTransition.springify()} style={{borderRadius: 1000, alignSelf: 'center',justifyContent: 'center',alignItems: 'center'}}>
+        <TouchableOpacity onPress={onPress} className="flex-row px-4 py-2 justify-center items-center">
+          <Text text={`${date?.month === todayMonth && date?.day === todayDay ? '오늘' : `${date?.month}월 ${date?.day}일`}의 기록`} type="body3" className="text-center" />
+          {/* <FileIcon width={14} height={16} color={'white'}/> */}
+        </TouchableOpacity>
+      </AnimatedLiquidGlassView>
   );
 };
+
+const AnimatedLiquidGlassView = Animated.createAnimatedComponent(LiquidGlassView);
