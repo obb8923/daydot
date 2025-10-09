e4eccbc13a5cacda2905f22e4101f5a81233a1f6
obb8923 <obb8923@naver.com>
2025-10-09 20:09:17 +0900

    .

---
+++ assets/store.ts
@@ -0,0 +1,48 @@
+const description_short_en = `
+A life is made of days.
+`
+const description_en = `
+Your life is a collection of dots.
+Each one holds a single day.
+
+80 dots for a lifetime,
+365 for a year.
+In each dot, there’s a piece of you.
+
+Write about your days,
+or simply watch them pass.
+One by one,
+they become your story.
+
+We often think we have more time than we do.
+But maybe not.
+See what remains—
+and feel today more deeply.
+
+Don’t just live the day.
+Leave your mark, one dot at a time.
+`;
+const description_ko = `
+당신의 인생은 점으로 이어져 있습니다.
+그 점 하나하나가, 당신의 하루입니다. 
+
+80개의 점으로 인생을, 
+365개의 점으로 올해를 바라보세요. 
+그 안에, 지금 당신이 있습니다. 
+
+하루를 기록하고, 
+때로는 그저 바라보세요. 
+작은 점 하나가 쌓여 
+당신의 이야기가 됩니다. 
+
+우리는 종종 시간이 많다고 믿지만, 
+그건 착각일지도 모릅니다. 
+남은 시간을 마주하고, 
+오늘을 조금 더 소중히 느껴보세요. 
+
+그냥 하루를 보내지 마세요. 
+당신의 점을, 당신의 시간을 남기세요.
+`;
+const description_short_ko = `
+당신의 하루가 모여 인생이 됩니다.
+`
